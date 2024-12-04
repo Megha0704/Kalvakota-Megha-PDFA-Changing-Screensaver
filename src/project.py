cc0e9e74a275c204cc612c63ec72e8378e8666b4
Megha Kalvakota <mxk190043@utdallas.edu>
2024-12-04 00:23:49 -0600

    Conditions to exit full screen or change the image displayed

--- src/project.py
+++ src/project.py
@@ -13,9 +13,21 @@ def main():
     pygame.init()
     pygame.display.set_caption("Changing Screensaver")
     screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+    folder_path = "artworks"
+    artworks = load_images(folder_path)
+    current_image = 0 
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    running == False
+                elif event.key == pygame.K_RIGHT:
+                    current_image = (current_image + 1) % len(artworks)
+                elif event.key == pygame.K_LEFT:
+                    current_image = (current_image - 1) %len(artworks)
 
-
-
-# Run the program
 if __name__ == "__main__":
     main()
