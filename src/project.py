e9fc5fc414720c39b38ecb8a84720eb02e14b45a
Megha Kalvakota <mxk190043@utdallas.edu>
2024-12-03 22:46:47 -0600

    Load new images into a file called "artworks" and find previous images

--- src/project.py
+++ src/project.py
@@ -2,10 +2,18 @@ import pygame
 import os
 import random
 
+def load_images(folder):
+    images = []
+    for file in os.lastdir(folder):
+        if file.endswith(('.png', '.jpg', '.jpeg')):
+            images.append(pygame.image.load(os.path.join(folder, file))))
+    return images
+
 def main():
     pygame.init()
     pygame.display.set_caption("Changing Screensaver")
     screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+
 
 
 # Run the program
