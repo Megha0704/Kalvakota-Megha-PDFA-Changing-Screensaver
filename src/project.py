65fa361d257d5c5e87bacbf67c5414073233ad83
Megha Kalvakota <mxk190043@utdallas.edu>
2024-12-04 10:17:44 -0600

    Bug fixes and rescaling of images so that they don't look stretched

--- src/project.py
+++ src/project.py
@@ -4,10 +4,19 @@ import random
 
 def load_images(folder):
     images = []
-    for file in os.lastdir(folder):
+    for file in os.listdir(folder):
         if file.endswith(('.png', '.jpg', '.jpeg')):
-            images.append(pygame.image.load(os.path.join(folder, file))))
+            images.append(pygame.image.load(os.path.join(folder, file)))
     return images
+
+def scale_image(image, screen_size):
+    img_width, img_height = image.get_size()
+    screen_width, screen_height = screen_size
+    scale_factor = min(screen_width / img_width, screen_height / img_height)
+    new_width = int(img_width * scale_factor)
+    new_height = int(img_height * scale_factor)
+    scaled_image = pygame.transform.scale(image, (new_width, new_height))
+    return scaled_image
 
 def main():
     pygame.init()
@@ -23,7 +32,7 @@ def main():
                 running = False
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_q:
-                    running == False
+                    running = False
                 elif event.key == pygame.K_RIGHT:
                     current_image = (current_image + 1) % len(artworks)
                 elif event.key == pygame.K_LEFT:
@@ -33,6 +42,6 @@ def main():
         screen.blit(image, (0, 0))
         pygame.display.flip()
     pygame.QUIT
-    
+
 if __name__ == "__main__":
     main()
